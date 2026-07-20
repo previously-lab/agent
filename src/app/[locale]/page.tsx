059c8f30743037b81f62b6c898d2494a38fde489
Alan Yuan <a@ldwid.com>
2026-07-21 00:17:12 +0800

    fix: reset chat state on persona switch via key prop

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -27,7 +27,7 @@ export default async function HomePage({
   const episodicData = await getEpisodicState();
 
   return (
-    <ChatPage>
+    <ChatPage key={persona || "default"}>
       <HeroSection personaId={persona} />
       <TimelinePanel
         initialData={{
